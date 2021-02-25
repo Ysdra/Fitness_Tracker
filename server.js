290e990b689b4e0e6ec617639e8107bc8d9be9fd
Ysdra <joseph.m.park@utexas.edu>
2021-02-24 19:54:09 -0600

    new deployment

--- server.js
+++ server.js
@@ -14,11 +14,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // mongoDB connect
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { 
-    useNewUrlParser: true 
-    , useFindAndModify: false
-});
-
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 // routes
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
