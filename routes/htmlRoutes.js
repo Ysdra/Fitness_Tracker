e9b5c50256f8dabc721f3d0983005c9d8bb07a93
Ysdra <joseph.m.park@utexas.edu>
2021-02-24 20:29:17 -0600

    edited the htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,6 +9,10 @@ router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
+router.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
